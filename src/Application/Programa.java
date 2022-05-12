3d8d9183b65c4eecd00eaf096c1e2805d377aeaf
Victor Barbosa Goveia <victor.barbosa.gov@gmail.com>
2022-05-12 17:23:45 -0300

    Entidade Departamento

---
+++ src/Application/Programa.java
@@ -0,0 +1,15 @@
+package Application;
+
+import Model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Books");
+		System.out.println(obj);
+		
+
+	}
+
+}
